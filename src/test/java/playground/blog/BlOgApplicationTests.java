2bca59d239088977ccdb5aa6b0712c6dd5ed55ce
khattab <mkhattab369@gmail.com>
2025-06-12 19:49:39 +0300

    first commit

---
+++ src/test/java/playground/blog/BlOgApplicationTests.java
@@ -0,0 +1,13 @@
+package playground.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlOgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
