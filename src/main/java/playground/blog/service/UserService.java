b128b710cdc69f3febfc879fe247324a81846898
khattab <mkhattab369@gmail.com>
2025-07-11 16:08:45 +0300

    comment for main user roles to be implemented with spring security phase of the application

--- src/main/java/playground/blog/service/UserService.java
+++ src/main/java/playground/blog/service/UserService.java
@@ -1,4 +1,34 @@
 package playground.blog.service;
 
+import playground.blog.dto.user.UserRequestDTO;
+import playground.blog.dto.user.UserResponseDTO;
+
+import java.util.List;
+
 public interface UserService {
+    UserResponseDTO createUser(UserRequestDTO userRequestDTO);
+    UserResponseDTO updateUser(UserRequestDTO userRequestDTO);
+    UserResponseDTO deleteUser(UserRequestDTO userRequestDTO);
+    UserResponseDTO getUserById(UserRequestDTO userRequestDTO);
+    List<UserResponseDTO> getUsers();
+
 }
+/*
+create user // belong to auth but now for user
+update user
+delete user
+get all users
+get users by id
+get user by criteria
+ */
+/*
+authenticate
+change password
+reset password
+ */
+/*
+verify email
+resend verify email
+activate/ deactivate user
+
+ */
